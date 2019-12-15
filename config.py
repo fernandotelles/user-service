a12cc7e2e6bbe1767da92f6a71f347c798d22ea0
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 16:47:46 -0300

    change db_path

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/user.db'
+db_path = os.path.join(os.path.dirname(__file__), 'user.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SECRET_KEY'] = 'afacdd3132289db23325aaa6'
 db = SQLAlchemy(app)
