cb696d99823b774dc1bc760ed9dd918b2b2acc84
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 16:38:54 -0300

    get port from environment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from config import app, db
 from flask_restful import Api
 from src.services.user_service import UserService, UserList
@@ -8,4 +9,5 @@ api.add_resource(UserList, '/v1/users/')
 api.add_resource(UserService, '/v1/users/<user_id>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
