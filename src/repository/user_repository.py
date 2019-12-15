7d89599767344181b0e9edcdd28026732b998fe6
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 10:55:53 -0300

    update user feature

--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -13,4 +13,10 @@ class UserRepository:
         user = self.get_by_id(id)
         db.session.delete(user)
         db.session.commit()
+    
+    def update(self, a_user):
+        user = self.get_by_id(a_user['id'])
+        user.name = a_user['name']
+        db.session.commit()
+
        
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -8,10 +8,20 @@ resource_fields = {
 
 repository = UserRepository()
 
+parser = reqparse.RequestParser()
+parser.add_argument('user_id', location='json')
+parser.add_argument('user_name', location='json')
+
 def abort_if_dont_exists(user_id):
     user = repository.get_by_id(user_id)
     if user == None:
         abort(404, message="User {} doesn't exist".format(user_id))
+
+def argsToDict(args):
+    user = {}
+    user['id'] = args['user_id']
+    user['name'] = args['user_name']
+    return user
 
 class UserService(Resource):
     @marshal_with(resource_fields)
@@ -22,6 +32,13 @@ class UserService(Resource):
     def delete(self, user_id):
         abort_if_dont_exists(user_id)
         repository.delete(user_id)
+        return '', 204
+    
+    def put(self, user_id):
+        abort_if_dont_exists(user_id)
+        args = parser.parse_args()
+        user = argsToDict(args)
+        repository.update(user)
         return '', 204
 
 class UserList(Resource):
