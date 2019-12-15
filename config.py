6a4914158c17abe17fd9c6a7afb2c7c156c28596
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 17:40:40 -0300

    enable CORS for flask

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 db_path = os.path.join(os.path.dirname(__file__), 'user.db')
@@ -8,3 +9,4 @@ db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SECRET_KEY'] = 'afacdd3132289db23325aaa6'
 db = SQLAlchemy(app)
+CORS(app)
